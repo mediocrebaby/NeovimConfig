c6c3dafdde0bd05fb27084e8e21e91d536a20220
JustSo <1009462586@qq.com>
2025-01-20 21:15:00 +0800

    feat:新增Obsidian.nvim的相关快捷键

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,4 +6,6 @@ if not vim.g.vscode then
   local map = vim.keymap.set
   map("n", "<leader>e", ":lua MiniFiles.open(vim.api.nvim_buf_get_name(0))<cr>", { desc = "Triggle Mini Files" })
   map("n", "<leader>os", ":ObsidianQuickSwitch<cr>", { desc = "Quick Switch Ohter Note In Obsidian Vault" })
+  map("n", "<leader>oo", ":ObsidianOpen<cr>", { desc = "Open Note In Obsidian App" })
+  map("n", "<leader>op", ":ObsidianPasteImg<cr>", { desc = "Paste Img" })
 end
