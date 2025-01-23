1f69f60a4da42cf0b4dbc23231c4c8071eff903b
mediocrebabay <initialcode111@gmail.com>
2025-01-23 14:48:32 +0800

    feat:obsidian.nvim新增日记和图片配置

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -16,5 +16,24 @@ return {
         path = "~/Documents/Note/knowledge",
       },
     },
+    completion = {
+      -- 启用补全
+      nvim_cmp = true,
+      -- 两个字符触发补全
+      min_chars = 2,
+    },
+    preferred_link_style = "wiki",
+
+    attachments = {
+      -- 设置默认的图片文件路径
+      img_folder = "~/Documents/Note/Knowledge/attachments",
+    },
+
+    daily_notes = {
+      folder = "每日任务",
+      data_format = "%Y-%m-%d",
+      default_tags = { "每日任务" },
+      template = nil,
+    },
   },
 }
