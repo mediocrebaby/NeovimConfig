77effb0d3bedf30e716e5a296f8366c997c84038
mediocrebaby <1009462586@qq.com>
2026-01-20 13:26:57 +0800

    fix: <leader>E 定位当前文件到 Neo-tree

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,5 +4,15 @@
 
 if not vim.g.vscode then
   local map = vim.keymap.set
+
   map("n", "<leader>e", "<cmd>Neotree toggle<cr>", { desc = "Explorer (Neo-tree)", noremap = true, silent = true })
+  map("n", "<leader>E", function()
+    require("neo-tree.command").execute({
+      action = "focus",
+      source = "filesystem",
+      reveal = true,
+      reveal_force_cwd = true,
+    })
+  end, { desc = "Explorer (Neo-tree) reveal current file", noremap = true, silent = true })
 end
+
